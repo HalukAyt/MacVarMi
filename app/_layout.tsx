b861a3a2d374d8d0142605d66264cb808baade85
HalukAyt <halukaytis@hotmail.com>
2025-09-26 18:23:36 +0300

    navbar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,43 @@
-import { Stack } from "expo-router";
+// app/_layout.tsx
+import { Tabs } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome"; // tercih: @expo/vector-icons
 
-export default function RootLayout() {
-  return <Stack />;
+export default function Layout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#900",
+        tabBarInactiveTintColor: "gray",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Ana Sayfa",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="soccer-ball-o" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="matches"
+        options={{
+          title: "Maçlar",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="calendar" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Ayarlar",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="cog" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
